6885cddc0882b49d5b212d5d0d7c19ab9285877d
Nicolas <nicoacerbi4@gmail.com>
2022-10-19 14:09:02 -0300

    Mejoras

--- Desafio-Clases/Desafio-Clases.js
+++ Desafio-Clases/Desafio-Clases.js
@@ -1,52 +1,36 @@
 class Usuario {
-	constructor(nombre, apellido, libros, mascotas) {
+	constructor(nombre, apellido) {
 		this.nombre = nombre;
 		this.apellido = apellido;
-		this.libros = libros; // objeto de libros
-		this.mascotas = mascotas; //array de objetos mascota.
+		this.libros = []; // objeto de libros
+		this.mascotas = []; //array de objetos mascota.
 	}
 	getFullName = () => {
 		// método que retorna el nombre y apellido, utilizando templates literales.
 		return `${this.nombre} ${this.apellido}`;
 	};
-	addMascotas = nombreMascota => {
-		// método que agrega una mascota al array de mascotas.
-		this.mascotas.push(nombreMascota);
-	};
-	countMascotas = () => {
-		// método que cuenta la cantidad de mascotas.
-		return this.mascotas.length;
-	};
-	addBook = (nombreLibro, autorLibro) => {
-		// método que agrega un objeto libro al array de libros.
-		this.libros.push({
-			nombreLibro: nombreLibro,
-			autorLibro: autorLibro
-		});
-	};
-	getBooks = () => {
-		// método que retorna los libros.
-		
-		return this.libros.map(libro => libro);
-	};
+    addMascota(nombreMascota){
+        this.mascotas.push(nombreMascota)
+    }
+    countMascota(){
+        return this.mascotas.length
+    }
+	addBook(titulo, autor){
+        const libroInf = {nombreLibro: titulo, nombreAutor: autor}
+        this.libros.push(libroInf)
+    }
 	getBookNames = () => {
 		// método que retorna los nombres de los libros.
 		return this.libros.map(libro => libro.nombreLibro);
 	};
 }
 
-const ricky = new Usuario( // usuario de Ricardo Fort
-	"Ricardo",
-	"Fort",
-	[
-		{ nombreLibro: "Padre rico, padre pobre", autorLibro: "Robert Kiyosaki" },
-		{ nombreLibro: "Piensa y hazte rico", autorLibro: "Napoleon Hill" }
-	],
-	["Tigre Marroc"] // mascota
-);
+const ricky = new Usuario("Ricardo","Fort")
 
 console.log("el usuario se llama: ", ricky.getFullName());
-ricky.addMascotas("Perro Chocolate");
-console.log("el usuario tiene: ", ricky.countMascotas(), " mascotas");
+ricky.addMascota("Perro Chocolate");
+ricky.addMascota("Tigre Marroc");
+console.log("el usuario tiene: ", ricky.countMascota(), " mascotas");
 ricky.addBook("Los secretos de la mente millonaria", "T. Harv Eker");
-console.log("El usuario tiene estos "+ricky.libros.length +" libros: ", ricky.getBooks());
+ricky.addBook("Padre Rico, Padre Pobre", "Robert Kiyosaki");
+console.log("El usuario tiene estos "+ricky.libros.length +" libros: ", ricky.getBookNames());
